a32ca996023860310ae7420bf10a26276c666ef1
JoaoRibeiro2 <joaoribeyro1@gmail.com>
2020-11-25 08:26:24 -0300

    Main file

--- App.js
+++ App.js
@@ -2,68 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView, TextInput } from 'react-native';
 
-export default class App extends React.Component{
-    constructor(props){
-      super(props);
-      this.state = {date : new Date()}
-    }
+import Routes from './src/routes'
 
-    relogio(){
-      this.setState({
-        date : new Date()
-      });
-  }
+export default function App(){
+  return (
 
-  componentDidMount(){
-    this.TimerID = setInterval(() => this.relogio(1000));
-  }
+        <Routes />
 
-  componentWillUnmount(){
-    clearInterval(this.TimerID);
-  }
-
-  render(){
-    return (
-      <View style={styles.container}>
-        <View style={styles.button}>
-          <Text style={styles.p}>{this.state.date.toLocaleTimeString()}</Text>
-        </View>
-      </View>
-
-    )
-  }
-
+  )
 }
-// export default function App () {
-//   return (
-//     <View style={styles.container}>
-//       <Text>ello, world!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button:{
-    width: 300,
-    height: 100,
-    borderRadius: 10,
-    backgroundColor: 'transparent',
-    color: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  p: {
-    color: '#00F7FF',
-    fontSize: 50, 
-    textShadowColor: 'rgba(0, 0, 0, 0.75)',
-    textShadowOffset: {width: -1, height: 1},
-    textShadowRadius: 10
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
   }
-});
+})
